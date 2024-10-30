debd72068a5de1444ba6ab868d5202d8dba24295
Lemur74 <75430880+Lemur74@users.noreply.github.com>
2024-10-31 01:16:15 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+count_homeworks = 12
+course = 'Python'
+all_time = 1.5
+time_for_one_hw = all_time/count_homeworks
+print('Курс:',course, ', всего задач:', count_homeworks, ', затрачено часов:', all_time, ', среднее время выполнения: ', time_for_one_hw)
